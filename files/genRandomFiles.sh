42b9b608fb7ddd4a424c356f3c24ce53334324b3
xph906 <xiangpan2011@u.northwestern.edu>
2016-04-22 09:27:33 -0500

    update

--- files/genRandomFiles.sh
+++ files/genRandomFiles.sh
@@ -3,4 +3,5 @@ python generateTextFileOfSpecificSize.py 5242880 fivem.txt
 python generateTextFileOfSpecificSize.py 1048576 onem.txt
 python generateTextFileOfSpecificSize.py 1024 onek.txt
 python generateTextFileOfSpecificSize.py 10240 tenk.txt
+python generateTextFileOfSpecificSize.py 10240 bw-sample.txt
 python generateTextFileOfSpecificSize.py 102400 hundredk.txt
